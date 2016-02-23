e584b102cc983c0b25da8088299f9f9b1b327e2c
Steve Miller <smiller6@jaguarlandrover.com>
2016-02-23 12:13:28 -0800

    WIP: can monitoring can be started from dbus

--- elm_dbus_main.py
+++ elm_dbus_main.py
@@ -43,8 +43,8 @@ class ElmDbus(dbus.service.Object):
                                             bus=dbus.SessionBus())
         self.obd = None
 
-        self.que_command = command_queue
-        self.que_response = response_queue
+        self._command_queue = command_queue
+        self._response_queue = response_queue
 
     ############################################################################
     #   dbus
@@ -58,36 +58,54 @@ class ElmDbus(dbus.service.Object):
     @dbus.service.method('rvi.vsi.ElmDbus')
     def at_command(self, msg=None):
         print ">" + msg
-        self.que_command.put
+        self._command_que.put(msg)
 
     @dbus.service.signal('rvi.vsi.ElmDbus')
     def can_response(self, msg=None):
         print(msg)
 
     @dbus.service.method('rvi.vsi.ElmDbus')
-    def monitor_can(self, silent=False, format=True, header=True, spaces=True):
-
-        #set parameters desired for can output
-        self.command_set_silent_monitor(silent)
-        self.command_format_can(format)
-        self.command_header_on(header)
-        self.command_spaces_on(spaces)
-        self.command_monitor_can_at()
+    def monitor_can(self, silent=False, format_can=True, header=True,
+                    spaces=True):
+        self._command_queue.put('ath1')
+        self._command_queue.put('atspa8')
+        self._command_queue.put('atcaf0')
+        self._command_queue.put('atcsm0')
+        self._command_queue.put('atdp')
+        self._command_queue.put('atma')
 
         #this line is problematic, we need a way to start monitoring/reading
         #without blocking the reply
         #read_elm(self)
-        return 'Starting can monitor'
-    ############################################################################
+        reply = 'Sending commands to monitor CAN, ' \
+                'silent={silent}, ' \
+                'format_can={format_can}' \
+                'header={header}' \
+                'spaces={spaces}'.format(silent=silent, format_can=format_can,
+                                         header=header, spaces=spaces)
+        print(reply)
+        return reply
 
-    ############################################################################
+
+################################################################################
+
+
+################################################################################
+class ElmObd(object):
+    def __init__(self, command_queue, response_queue):
+        object.__init__(self)
+
+        self.obd = None
+        self._command_queue = command_queue
+        self._response_queue = response_queue
+        self.response = None
     #commands
     #   commands for interacting directly with ELM/STN chip
     #   ST specific commands are designated with the postfix _st
     #   per the ELM/STN set, most commands take a single argument
 
     def command_at_command(self, command=""):
-        pass
+        return self.obd._send_command(command)
 
     def command_set_silent_monitor(self, silent=True):
         if silent:
@@ -134,57 +152,51 @@ class ElmDbus(dbus.service.Object):
     def command_select_protocol_auto(self, protocol_number="0"):
         return self.obd._send_command("ATSPA" + protocol_number)
 
+    def start_elm(self, serial_device=None, baud_rate=0):
+        if SERIAL_DEVICE:
+            if BAUD_RATE >= 9600 and BAUD_RATE <= 1000000:
 
-    ############################################################################
+                ser = SerialConnectionFactory(port_class=Serial,
+                                              baudrate=9600,
+                                              parity=PARITY_NONE,
+                                              timeout=0.5,
+                                              write_timeout=0.1)
 
-class ElmObd(object):
-    def __init__(self):
-        object.__init__(self)
-        
-        self.obd = None
+                connection = ser.connect(SERIAL_DEVICE)
 
-def start_elm(ElmDbus, ElmObd, serial_device=None, baud_rate=0):
-    if SERIAL_DEVICE:
-        if BAUD_RATE >= 9600 and BAUD_RATE <= 1000000:
+                self.obd = OBDInterface(connection)
 
-            ser = SerialConnectionFactory(port_class=Serial,
-                                          baudrate=9600,
-                                          parity=PARITY_NONE,
-                                          timeout=0.5,
-                                          write_timeout=0.1)
+                self._response_queue.put(self.command_at_command("ATI"))
+                self._response_queue.put(self.command_at_command("ATE0"))
+                self._response_queue.put(
+                    self.command_at_command("STSBR" + str(BAUD_RATE)))
 
-            connection = ser.connect(SERIAL_DEVICE)
+                # close connection, reconnect
+                self.obd._connection.close()
 
-            ElmObd.obd = OBDInterface(connection)
+                ser = SerialConnectionFactory(port_class=Serial,
+                                              baudrate=BAUD_RATE,
+                                              parity=PARITY_NONE,
+                                              timeout=0.5,
+                                              write_timeout=0.1)
 
-            # print((obd._send_command("ATZ")))
+                connection = ser.connect(SERIAL_DEVICE)
 
-            ElmDbus.at_response(ElmObd.obd._send_command("ATI"))
-            ElmDbus.at_response(ElmObd.obd._send_command("ATE0"))
-            ElmDbus.at_response(ElmObd.obd._send_command("STSBR" + str(BAUD_RATE)))
+                self.obd = OBDInterface(connection)
+                response = self.command_at_command("STI")
+                response_queue.put(response)
 
-            #close connection, reconnect
-            ElmObd.obd._connection.close()
 
-            ser = SerialConnectionFactory(port_class=Serial,
-                                          baudrate=BAUD_RATE,
-                                          parity=PARITY_NONE,
-                                          timeout=0.5,
-                                          write_timeout=0.1)
-
-            connection = ser.connect(SERIAL_DEVICE)
-
-            ElmObd.obd = OBDInterface(connection)
-            response = ElmObd.obd._send_command("STI")
-            ElmDbus.at_response(response)
-
+            else:
+                print("Baud rate incorrect. Supply range 9600:1000000")
 
         else:
-            print("Baud rate incorrect. Supply range 9600:1000000")
+            print("Insufficient args provided.\n"
+                  "Please supply serial device and baudrate\n")
 
-    else:
-        print("Insufficient args provided.\n"
-              "Please supply serial device and baudrate\n")
+
+################################################################################
+
 
 # def read_elm(ElmDbus):
 #     while(True):
@@ -205,13 +217,14 @@ def run_elm_obd(ElmObd):
         else:
             elm_response = ElmObd.obd._connection._read()
 
-        if (elm_response is not None):
+        # we don't care about responses that have no info
+        if elm_response is not None and elm_response is not '':
             response_queue.put(elm_response)
             
 
 def run_dbus(ElmDbus):
     while(True):
-        if(ElmDbus.que_response.empty() == False):
+        if (response_queue.empty() == False):
             #publish response
             response = response_queue.get()
             ElmDbus.at_response(response)
@@ -221,41 +234,32 @@ def run_dbus(ElmDbus):
 if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     # create dbus object
-    elm_obj = ElmDbus(dbus.SessionBus())
-    elm_obd = ElmObd()
+    elm_dbus = ElmDbus(dbus.SessionBus())
+    elm_obd = ElmObd(command_queue, response_queue)
     # start thread on func
 
     # blocking setup call to start elm chip for the first time
-    start_elm(elm_obj, elm_obd, SERIAL_DEVICE, BAUD_RATE)
+    elm_obd.start_elm(SERIAL_DEVICE, BAUD_RATE)
 
     # we assume elm has started and we can talk to it...
 
-    # set up for can monitor
-
-    # test of command response:
+    # example/test of command response:
     send_command(command_queue, "atws")
     command = command_queue.get()
     command_queue.task_done()
-    print(elm_obj.obd._send_command(command))
-    response = elm_obj.obd._connection._read()
+    print(elm_obd.obd._send_command(command))
+    response = elm_obd.obd._connection._read()
     response_queue.put(response)
-    elm_obj.at_response(response_queue.get())
+    elm_dbus.at_response(response_queue.get())
     response_queue.task_done()
 
-    send_command(elm_obj.que_command, 'ath1')
-    send_command(elm_obj.que_command, 'atspa8')
-    send_command(elm_obj.que_command, 'atcaf0')
-    send_command(elm_obj.que_command, 'atcsm0')
-    send_command(elm_obj.que_command, 'atdp')
-    send_command(elm_obj.que_command, 'atma')
-
     elm_thread = threading.Thread(target=run_elm_obd, args=(elm_obd,))
-    elm_dbus_thread = threading.Thread(target=run_dbus, args=(elm_obj,))
+    elm_dbus_thread = threading.Thread(target=run_dbus, args=(elm_dbus,))
 
     elm_thread.start()
     elm_dbus_thread.start()
 
-    #elm_obj.monitor_can(silent=False, format=True, header=True, spaces=True)
+    # elm_dbus.monitor_can(silent=False, format_can=True, header=True, spaces=True)
 
     print('Starting GTK Main')
     gtk.main()
